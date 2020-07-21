b5363e03d017b40e49cc90f1dd10236d8d3e4855
enochyuan <enochyuan@tencent.com>
2020-07-21 19:14:21 +0800

    设置样式

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -19,8 +19,8 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-     var aa = wx.getFileSystemManager().readFileSync("images/homePage.png","base64");
-     console.log(aa);
+    //  var aa = wx.getFileSystemManager().readFileSync("images/homePage.png","base64");
+    //  console.log(aa);
   },
 
   /**
